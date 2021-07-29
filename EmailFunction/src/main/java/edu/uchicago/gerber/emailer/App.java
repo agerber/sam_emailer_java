027184bc8f917120efedbe00cdf66c6331dfde24
Adam Gerber <adam.gerber@yahoo.com>
2021-07-29 00:54:43 -0500

    pass json back to sender

--- EmailFunction/src/main/java/edu/uchicago/gerber/emailer/App.java
+++ EmailFunction/src/main/java/edu/uchicago/gerber/emailer/App.java
@@ -14,6 +14,7 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONObject;
 
 /**
  * Handler for requests to Lambda function.
@@ -38,9 +39,16 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
                     .withBody(String.format("{%s}", e.getMessage()))
                     .withStatusCode(400);
         }
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("subject", message.getSubject());
+        jsonObject.put("body", message.getBody());
+        jsonObject.put("email", message.getEmail());
+
+
         return response
                 .withStatusCode(200)
-                .withBody("{}");
+                .withBody(jsonObject.toString());
 
 
     }
