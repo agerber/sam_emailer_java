c6b7f6ec451475fc416c470297d01b0fd24834da
Adam Gerber <adam.gerber@yahoo.com>
2021-07-29 00:51:37 -0500

    create logic for App

--- EmailFunction/src/main/java/edu/uchicago/gerber/emailer/App.java
+++ EmailFunction/src/main/java/edu/uchicago/gerber/emailer/App.java
@@ -12,11 +12,15 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Handler for requests to Lambda function.
  */
 public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
         Map<String, String> headers = new HashMap<>();
@@ -25,24 +29,21 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
 
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
                 .withHeaders(headers);
+
+        Message message = null;
         try {
-            final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
-            String output = String.format("{ \"message\": \"hello world\", \"location\": \"%s\" }", pageContents);
-
+             message = objectMapper.readValue(input.getBody(), Message.class);
+        } catch (JsonProcessingException e) {
             return response
-                    .withStatusCode(200)
-                    .withBody(output);
-        } catch (IOException e) {
-            return response
-                    .withBody("{}")
-                    .withStatusCode(500);
+                    .withBody(String.format("{%s}", e.getMessage()))
+                    .withStatusCode(400);
         }
+        return response
+                .withStatusCode(200)
+                .withBody("{}");
+
+
     }
 
-    private String getPageContents(String address) throws IOException{
-        URL url = new URL(address);
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
-            return br.lines().collect(Collectors.joining(System.lineSeparator()));
-        }
-    }
+
 }
